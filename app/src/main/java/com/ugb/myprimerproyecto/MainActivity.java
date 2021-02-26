f61f483a57e0ab788cb63e5650b049b593ad7932
alexwam03 <alexwam03@outlook.com>
2021-02-26 08:56:23 -0600

    Sensor de proximidad

--- app/src/main/java/com/ugb/myprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/myprimerproyecto/MainActivity.java
@@ -2,83 +2,82 @@ package com.ugb.myprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    SensorManager administradorsensor;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    TextView tempVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempVal = (findViewById(R.id.lblSensor));
+        activarsensor();
     }
 
-    public void calcular(View view){
-double num1 = 0;
-double num2 = 0;
-double respuesta = 0;
+    private void activarsensor() {
+administradorsensor = (SensorManager)getSystemService(SENSOR_SERVICE);
+sensor = administradorsensor.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        TextView temnun = (TextView)findViewById(R.id.txtnum1);
-        String valor1 = temnun.getText().toString();
-        if (valor1.matches("")) {
-            num1 = 0;
-        }   else {
-            num1 = Double.parseDouble(temnun.getText().toString());
-        }
+if (sensor == null){
+    Toast.makeText(getApplicationContext(), "No dipones de un sensor de proximindad", Toast.LENGTH_LONG).show();
+    finish();
+}
 
-        temnun = (TextView)findViewById(R.id.txtnum2);
-        String valor2 = temnun.getText().toString();
-        if (valor2.matches("")){
-             num2 = 0;
-        } else {
-             num2 = Double.parseDouble(temnun.getText().toString());
-        }
+sensorEventListener = new SensorEventListener() {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+ tempVal.setText("Valor: "+event.values[0] );
+ if ( event.values[0]<4) {
+     getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+    } else if (event.values[0]>=4 && event.values[0]<8){
+     getWindow().getDecorView().setBackgroundColor(Color.RED);
+ } else {
+     getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+ }
 
-
-
-
-        Spinner operaciones = findViewById(R.id.coboperaciones);
-        switch (operaciones.getSelectedItemPosition()){
-           case 0: //suma
-                respuesta = num1 + num2;
-                    break;
-           case 1: //resta
-                respuesta = num1 - num2;
-                    break;
-           case 2: //multiplicacion
-                respuesta = num1 * num2;
-                    break;
-           case 3: //divicion
-                respuesta = num1 / num2;
-               break;
-            case 4: //factorial
-
-                respuesta = 1;
-                for(int i=1; i<=num1; i++){
-                    respuesta = i * respuesta;}
-                break;
-            case 5: //porcentaje
-                respuesta = (num2/100)*num1;
-                break;
-            case 6: //exponenciacion
-                respuesta = Math.pow(num1,num2);
-                break;
-            case 7: //raiz
-                respuesta = Math.pow(num1, 1/num2); //la raiz x de un numero x es igual a ese numero elevado a 1/numeroderaiz
-                break;
-            case 8://modulo
-                respuesta = num1 % num2; // & es el resto en java
-                break;
-            case 9://mayor de
-                if (num1 >= num2){
-                    respuesta = num1;
-                } else {respuesta= num2;}
-                break;
-        }
-        temnun = findViewById(R.id.resultado);
-        temnun.setText("Respuesta " + respuesta);
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+};
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+
+    private void iniciar(){
+        administradorsensor.registerListener(sensorEventListener,sensor,2000*1000);
+    }
+    private void detener(){
+        administradorsensor.unregisterListener(sensorEventListener);
+    }
+
+
+
 }
