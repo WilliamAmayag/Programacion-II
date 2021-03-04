61d2b80eb5bf3d94176c6c2833593dc7e96a97ff
Brian-Nel <78221573+Brian-Nel@users.noreply.github.com>
2021-03-04 11:53:07 -0600

    Creación de tabhost y diseño base.

--- app/src/main/java/com/ugb/myprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/myprimerproyecto/MainActivity.java
@@ -2,16 +2,27 @@ package com.ugb.myprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
 
 
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout Contenido;
+    TabHost tabhost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Contenido = findViewById(R.id.contenidoView);
+        tabhost = findViewById(R.id.tbhgeneral);
+        tabhost.setup();
+
+        tabhost.addTab(tabhost.newTabSpec("valoragua").setContent(R.id.valoragua).setIndicator("agua"));
+        tabhost.addTab(tabhost.newTabSpec("conversorarea").setContent(R.id.tabconversorarea).setIndicator("area"));
 
     }
 }
